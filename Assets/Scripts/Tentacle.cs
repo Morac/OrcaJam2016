25526ca799b4ce62e94d9279666e73e660c50d59
Max Loy <morac@morac.net>
2016-10-29 16:45:27 -0700

    Implemented tentacles

---
+++ Assets/Scripts/Tentacle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class Tentacle : MonoBehaviour
+{
+	public float RotateVarianceMin = 10;
+	public float RotateVarianceMax = 40;
+	public float RotateSpeedMin = 1;
+	public float RotateSpeedMax = 3;
+
+	public float ScaleSpeedMin = 2;
+	public float ScaleSpeedMax = 5;
+
+	void Start()
+	{
+		transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+
+		var angleVar = Random.Range(RotateVarianceMin, RotateVarianceMax);
+		var speed = Random.Range(RotateSpeedMin, RotateSpeedMax);
+
+		transform.DORotate(transform.rotation.eulerAngles + new Vector3(0, 0, angleVar), speed).SetLoops(-1, LoopType.Yoyo);
+
+		transform.DOScaleX(transform.localScale.x * 2, Random.Range(ScaleSpeedMin, ScaleSpeedMax)).SetLoops(-1, LoopType.Yoyo);
+	}
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,6 +6,12 @@ public class UIManager : Singleton<UIManager>
 {
 	public Image Fadeout;
 
+	void Start()
+	{
+		Fadeout.color = new Color(Fadeout.color.r, Fadeout.color.g, Fadeout.color.b, 1);
+		Fadeout.DOFade(0f, 0.5f);
+	}
+
 	public void DoFade()
 	{
 		Fadeout.DOFade(1f, 2f).SetUpdate(true);
