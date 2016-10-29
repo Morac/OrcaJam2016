171d14b71414082be8341aea0ab7f7d42381b494
Max Loy <morac@morac.net>
2016-10-29 13:33:24 -0700

    Added sky

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using DG.Tweening;
+using System.Collections.Generic;
 
 public class GameManager : Singleton<GameManager>
 {
 	public FancyPlayerMover Player;
+
+	public List<Obstacle> Obstacles = new List<Obstacle>();
+	public float ObstacleDensity = 1;
+
+	public List<Target> Targets = new List<Target>();
+	public float TargetDensity = 5;
+
+	public float UpdateRange = 10;
+	public float MaxY = 0;
+	
+
 
 	public bool GameStarted { get; private set; }
 
@@ -27,17 +39,32 @@ public class GameManager : Singleton<GameManager>
 			if(Player.enabled && Player.transform.position.y > 0 && Player.caughtTarget != null)
 			{
 				//end game
-				Player.enabled = false;
-
-				Player.victoryParticles.Play();
-
-				//high score??
-				var seq = DOTween.Sequence();
-				seq.AppendInterval(2);
-				seq.AppendCallback(() => UIManager.Instance.DoFade());
-				seq.AppendInterval(2);
-				seq.AppendCallback(() => UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex));
+				EndGame();
+			}
+			else
+			{
+				UpdateGameWorld();
 			}
 		}
+	}
+
+	void EndGame()
+	{
+		Player.enabled = false;
+
+		Player.victoryParticles.Play();
+
+		//high score??
+		var seq = DOTween.Sequence();
+		seq.AppendInterval(2);
+		seq.AppendCallback(() => UIManager.Instance.DoFade());
+		seq.AppendInterval(2);
+		seq.AppendCallback(() => UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex));
+	}
+
+	//spawn nearby items if needed
+	void UpdateGameWorld()
+	{
+		
 	}
 }
