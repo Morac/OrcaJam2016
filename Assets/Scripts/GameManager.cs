99822c2f002cc95d44e78600dbe4860a2c44f610
Max Loy <morac@morac.net>
2016-10-29 14:06:34 -0700

    Procedural generation.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,16 +6,18 @@ public class GameManager : Singleton<GameManager>
 {
 	public FancyPlayerMover Player;
 
-	public List<Obstacle> Obstacles = new List<Obstacle>();
+	public List<GameObject> Obstacles = new List<GameObject>();
 	public float ObstacleDensity = 1;
 
-	public List<Target> Targets = new List<Target>();
+	public List<GameObject> Targets = new List<GameObject>();
 	public float TargetDensity = 5;
 
 	public float UpdateRange = 10;
+	public float PositionVariance = 2;
 	public float MaxY = 0;
-	
 
+	Dictionary<Vector2, GameObject> SpawnedObstacles = new Dictionary<Vector2, GameObject>();
+	Dictionary<Vector2, GameObject> SpawnedTargets = new Dictionary<Vector2, GameObject>();
 
 	public bool GameStarted { get; private set; }
 
@@ -41,11 +43,10 @@ public class GameManager : Singleton<GameManager>
 				//end game
 				EndGame();
 			}
-			else
-			{
-				UpdateGameWorld();
-			}
 		}
+
+		UpdateSpawnedObjects(ObstacleDensity, Obstacles, SpawnedObstacles);
+		UpdateSpawnedObjects(TargetDensity, Targets, SpawnedTargets);
 	}
 
 	void EndGame()
@@ -62,9 +63,26 @@ public class GameManager : Singleton<GameManager>
 		seq.AppendCallback(() => UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex));
 	}
 
-	//spawn nearby items if needed
-	void UpdateGameWorld()
+	void UpdateSpawnedObjects(float density, List<GameObject> prefabs, Dictionary<Vector2, GameObject> spawnedDictionary)
 	{
-		
+		var pos_x = Player.transform.position.x - Player.transform.position.x % density;
+		var pos_y = Player.transform.position.y - Player.transform.position.y % density;
+
+		for(float x = pos_x - UpdateRange; x < pos_x + UpdateRange; x += density)
+		{
+			for(float y = pos_y - UpdateRange; y < pos_y + UpdateRange && y < MaxY; y += density)
+			{
+				Vector2 pos = new Vector2(x, y);
+				if(spawnedDictionary.ContainsKey(pos))
+				{
+					continue;
+				}
+
+				var inst = Instantiate(prefabs[Random.Range(0, prefabs.Count)]);
+				inst.transform.position = pos + new Vector2(Random.Range(-PositionVariance, PositionVariance), Random.Range(-PositionVariance, PositionVariance));
+
+				spawnedDictionary[pos] = inst;
+			}
+		}
 	}
 }
