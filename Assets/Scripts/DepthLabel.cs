a466002e6ea91ea9efcf3d98d124b6d4559b794d
Max Loy <morac@morac.net>
2016-10-29 16:22:53 -0700

    Added depth label

---
+++ Assets/Scripts/DepthLabel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class DepthLabel : MonoBehaviour
+{
+	public Transform Player;
+
+	Text theText;
+
+	void Start()
+	{
+		theText = GetComponent<Text>();
+	}
+
+	void Update()
+	{
+		theText.text = (-Player.position.y).ToString("0.0") + " m";
+	}
+}
