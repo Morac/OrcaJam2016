a70bc3bcb441b33738ebb742b4ca2540caee7f74
Max Loy <morac@morac.net>
2016-10-29 17:18:38 -0700

    Teeth in the darkness

---
+++ Assets/Scripts/TeethInTheDarkness.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TeethInTheDarkness : MonoBehaviour
+{
+	public Transform Teeth;
+
+	public float DistanceModifier = 0.1f;
+
+	void Update()
+	{
+		var playerpos = GameManager.Instance.Player.transform.position;
+
+		var dist = (transform.position - playerpos).sqrMagnitude;
+
+		float scale = 1 / Mathf.Pow(dist, DistanceModifier);
+		scale = Mathf.Clamp01(scale);
+		Teeth.localScale = new Vector3(1, scale, 1);
+	}
+}
