dc3beb59cf8a119509caae53903e48d567547c2d
Max Loy <morac@morac.net>
2016-10-30 13:18:14 -0700

    Stubbed out Title scene

---
+++ Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class LoadSceneButton : MonoBehaviour
+{
+    public int SceneID = 1;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        SceneManager.LoadScene(SceneID);
+    }
+}
