b3d04e5d036013daf161d66c97de46e50f34f9eb
Max Loy <morac@morac.net>
2016-10-30 17:35:51 -0700

    Title scene sounds

--- Assets/Scripts/LoadSceneButton.cs
+++ Assets/Scripts/LoadSceneButton.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 [RequireComponent(typeof(Button))]
 public class LoadSceneButton : MonoBehaviour
 {
-    public int SceneID = 1;
+	public int SceneID = 1;
+	public Image Fade;
+	public AudioSource ambience;
 
-    void Start()
-    {
-        GetComponent<Button>().onClick.AddListener(OnClick);
-    }
+	void Start()
+	{
+		GetComponent<Button>().onClick.AddListener(OnClick);
+	}
 
-    void OnClick()
-    {
-        SceneManager.LoadScene(SceneID);
-    }
+	void OnClick()
+	{
+		ambience.DOFade(0, 1);
+		Fade.DOFade(1, 2).OnComplete(() => SceneManager.LoadScene(SceneID));
+	}
 }
