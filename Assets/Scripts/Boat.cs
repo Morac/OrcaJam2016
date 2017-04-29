83420f6ca437d8aa135c8cffdeed0b8ddec3462b
Morac <morac@morac.net>
2017-04-29 15:15:24 -0700

    Fixed fishing line by changing it to a LineRenderer. Adjusted draw ordering on other sprites.

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -4,8 +4,10 @@
 public class Boat : Singleton<Boat>
 {
 	public Transform Player;
-	public Transform FishingLine;
+	//public Transform FishingLine;
 	public Transform FishingLineMountPoint;
+
+	public LineRenderer FishingLine;
 
 	public float positionDamp = 1;
 	public float positionOffset = 4;
@@ -19,22 +21,23 @@ public class Boat : Singleton<Boat>
 		anim = GetComponent<Animator>();
 	}
 
-	void Update()
+	void LateUpdate()
 	{
 		var x = Mathf.SmoothDamp(transform.position.x, Player.position.x + positionOffset, ref posDampVelocity, positionDamp);
 
 		transform.position = new Vector3(x, transform.position.y, transform.position.z);
 
-		var mid = Player.position - FishingLineMountPoint.position;
-		mid /= 2;
-		mid = FishingLineMountPoint.position + mid;
+		//var mid = Player.position - FishingLineMountPoint.position;
+		//mid /= 2;
+		//mid = FishingLineMountPoint.position + mid;
 
-		FishingLine.position = mid;
+		//FishingLine.position = mid;
 
-		float scale = (Player.position - FishingLineMountPoint.position).magnitude * 9;
-		FishingLine.localScale = new Vector3(FishingLine.localScale.x, scale, FishingLine.localScale.z);
+		//float scale = (Player.position - FishingLineMountPoint.position).magnitude * 9;
+		//FishingLine.localScale = new Vector3(FishingLine.localScale.x, scale, FishingLine.localScale.z);
 
-		FishingLine.up = (FishingLineMountPoint.position - Player.position).normalized;
+		//FishingLine.up = (FishingLineMountPoint.position - Player.position).normalized;
+		FishingLine.SetPositions(new Vector3[] { Player.position, FishingLineMountPoint.position });
 	}
 
 	public void PlayEndAnimation()
